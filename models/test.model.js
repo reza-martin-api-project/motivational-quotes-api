fc4f7dfe62b49403f3e5b38e2dabac3171e4ebbc
Martin <martin.nguyen.20289@gmail.com>
2021-01-28 23:03:27 -0500

    established database connection

--- models/test.model.js
+++ models/test.model.js
@@ -6,13 +6,13 @@ const Schema = mongoose.Schema;
 const testSchema = new Schema(
     {
         quote: { type: String, required: true, unique: true },
-        author: { type: String, required: false }
+        author: { type: String, required: true }
     },
     {
         timestamps: true
     }
 );
 
-const Test = mongoose.model('Test', testSchema); // capital letter is for MongoDB to create new collection
+const Test = mongoose.model('test', testSchema); // capital letter is for MongoDB to create new collection
 
 module.exports = Test; // export default Test
--- route.js
+++ route.js
@@ -5,12 +5,8 @@ const Test = require('./models/test.model.js');
 // Basic requests
 router.route('/') // localhost
 .get( async (req, res) => {
-    try {
-        const findData = await Test.findOne({ author: "Nelson Mandela" });
-        res.status(200).json(findData);
-    } catch (err) {
-        console.log(err);
-    };
+    const findData = await Test.findOne({ author: 'Nelson Mandela' });
+    res.status(200).json(findData);
 })
 
 module.exports = router;
